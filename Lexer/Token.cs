7d800aad37812aa2681086d1f464312c907fd75e
Melodi <62381002+Melodi17@users.noreply.github.com>
2022-01-17 01:40:55 +1000

    update

--- Lexer/Token.cs
+++ Lexer/Token.cs
@@ -1,22 +1,23 @@
-﻿namespace Ream;
-
-public class Token
+﻿namespace Ream
 {
-    public readonly TokenType Type;
-    public readonly string Raw;
-    public readonly object Value;
-    public readonly int Line;
-
-    public Token(TokenType type, string raw, object value, int line)
+    public class Token
     {
-        Type = type;
-        Raw = raw;
-        Value = value;
-        Line = line;
-    }
+        public readonly TokenType Type;
+        public readonly string Raw;
+        public readonly object Value;
+        public readonly int Line;
 
-    public override string ToString()
-    {
-        return $"{Type} {Raw} {Value}";
+        public Token(TokenType type, string raw, object value, int line)
+        {
+            Type = type;
+            Raw = raw;
+            Value = value;
+            Line = line;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type} {Raw} {Value}";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,53 +1,55 @@
-﻿namespace Ream;
-public class Program
+﻿namespace Ream
 {
-    public static bool ErrorOccured = false;
-    public static void Main(string[] args)
+    public class Program
     {
-        if (args.Any())
-            RunFile(args.First());
-        else
-            RunPrompt();
-    }
-
-    private static void RunFile(string path)
-    {
-        Run(File.ReadAllText(path));
-
-        if (ErrorOccured)
-            Environment.Exit(65);
-    }
-    
-    private static void RunPrompt()
-    {
-        while (true)
+        public static bool ErrorOccured = false;
+        public static void Main(string[] args)
         {
-            Console.Write("> ");
-            string line = Console.ReadLine();
-            if (line == null) break;
-            Run(line);
-            ErrorOccured = false;
+            if (args.Any())
+                RunFile(args.First());
+            else
+                RunPrompt();
         }
-    }
 
-    private static void Run(string source)
-    {
-        Lexer lexer = new(source);
-        List<Token> tokens = lexer.Lex();
-
-        foreach (Token tok in tokens)
+        private static void RunFile(string path)
         {
-            Console.WriteLine(tok);
+            Run(File.ReadAllText(path));
+
+            if (ErrorOccured)
+                Environment.Exit(65);
         }
-    }
 
-    public static void Error(int line, string message)
-    {
-        Report(line, message);
-    }
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string line = Console.ReadLine();
+                if (line == null) break;
+                Run(line);
+                ErrorOccured = false;
+            }
+        }
 
-    public static void Report(int line, string message)
-    {
-        Console.Error.WriteLine($"Error on line {line}: {message}");
+        private static void Run(string source)
+        {
+            Lexer lexer = new(source);
+            //List<Token> tokens = lexer.Lex();
+
+            //foreach (Token tok in tokens)
+            //{
+            //    Console.WriteLine(tok);
+            //}
+        }
+
+        //public static void Error(int line, string message)
+        //{
+        //    Report(line, message);
+        //}
+
+        //public static void Report(int line, string message)
+        //{
+        //    Console.Error.WriteLine($"Error on line {line}: {message}");
+        //}
     }
 }
